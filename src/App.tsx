4b986dd3654937f3525361530548fd455e1c1fcc
AppEBD <ebdescueladominical@gmail.com>
2026-03-12 16:23:45 -0600

    Update App.tsx
    nievo inicio

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,19 @@ import Login from './pages/auth/Login';
 import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
+  // Leemos si existe la sesión en el almacenamiento local del navegador
   const session = localStorage.getItem('ebd_v2_session');
-  return session ? <Dashboard /> : <Login />;
+
+  return (
+    <div className="min-h-screen bg-slate-100 text-slate-800 font-sans">
+      {/* Lógica condicional (Ternaria):
+          ¿Existe sesión? 
+          SI -> Muestra el Dashboard
+          NO -> Muestra el Login
+      */}
+      {session ? <Dashboard /> : <Login />}
+    </div>
+  );
 }
+
+export default App;
