51021f7ee7efeb4166bb527a9363de0ef628eb8c
AppEBD <ebdescueladominical@gmail.com>
2026-03-12 17:30:37 -0600

    Update App.tsx
    estetico

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,11 @@ import Login from './pages/auth/Login';
 import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
-  // Leemos si existe la sesión en el almacenamiento local del navegador
   const session = localStorage.getItem('ebd_v2_session');
 
   return (
-    <div className="min-h-screen bg-slate-100 text-slate-800 font-sans">
-      {/* Lógica condicional (Ternaria):
-          ¿Existe sesión? 
-          SI -> Muestra el Dashboard
-          NO -> Muestra el Login
-      */}
+    /* Forzamos a que toda la aplicación sea un contenedor centrado absoluto */
+    <div className="min-h-screen w-full bg-slate-100 text-slate-800 font-sans flex flex-col items-center justify-center overflow-x-hidden">
       {session ? <Dashboard /> : <Login />}
     </div>
   );
