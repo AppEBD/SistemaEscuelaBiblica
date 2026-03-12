27bcff42f50888314a36dee2f7d4415381fd4915
AppEBD <ebdescueladominical@gmail.com>
2026-03-12 13:28:56 -0600

    Integrate AppRouter into the main App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,11 @@
+import { AppRouter } from './routes/AppRouter';
 
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-100 text-slate-800 font-sans">
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
