dcb95d3ec46db9d3199e50fa9e75857f9ce74042
AppEBD <ebdescueladominical@gmail.com>
2026-03-12 13:28:35 -0600

    Update AppRouter.tsx
    enreutador

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,1 +1,21 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthStore } from '../features/auth/store/authStore';
 
+export const AppRouter = () => {
+  const { usuarioActual } = useAuthStore();
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Si no hay usuario, lo mandamos al Login */}
+        <Route path="/login" element={<div className="p-10 text-center">Pantalla de Login (En construcción)</div>} />
+        
+        {/* Ruta temporal para pruebas */}
+        <Route path="/dashboard" element={<div className="p-10 text-center">Panel Principal (En construcción)</div>} />
+        
+        {/* Redirección por defecto */}
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
