18724eb341a8fbbed6b1ce87a651657e26989a96
AppEBD <ebdescueladominical@gmail.com>
2026-03-12 15:39:00 -0600

    Update main.tsx
    coloeres

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css' // <--- ¡Agrégala de nuevo! Ahora sí funcionará
 import App from './App.tsx'
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
