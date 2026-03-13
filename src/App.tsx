19e406615af93472ec3cf6c16d98fa8aa430ca9a
AppEBD <ebdescueladominical@gmail.com>
2026-03-12 18:05:45 -0600

    Update App.tsx
    corrección

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import Login from './pages/auth/Login';
 import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
-  const session = localStorage.getItem('ebd_v2_session');
+  const sessionStr = localStorage.getItem('ebd_v2_session');
+  let isApproved = false;
+
+  // Verificamos si existe la sesión Y si el estado es exactamente 'approved'
+  if (sessionStr) {
+    const user = JSON.parse(sessionStr);
+    if (user.status === 'approved') {
+      isApproved = true;
+    }
+  }
 
   return (
-    // w-full asegura que tome el 100% del monitor o pantalla del móvil
-    <div className="w-full min-h-screen font-sans text-slate-800">
-      {session ? <Dashboard /> : <Login />}
+    <div className="w-full min-h-screen font-sans text-slate-800 bg-slate-100 flex flex-col items-center justify-center">
+      {/* Si está 100% aprobado entra al Dashboard. Si está pendiente o no existe, va al Login */}
+      {isApproved ? <Dashboard /> : <Login />}
     </div>
   );
 }
