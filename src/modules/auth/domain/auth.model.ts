959fce8eb8e76e3ffc9616f42105aa9ca772becf
AppEBD <ebdescueladominical@gmail.com>
2026-03-19 19:44:19 -0600

    Update auth.model.ts

--- src/modules/auth/domain/auth.model.ts
+++ src/modules/auth/domain/auth.model.ts
@@ -1,12 +1,1 @@
-export type UserRole = 'ADMIN' | 'MAESTRO' | 'AUXILIAR' | 'LOGISTICA' | 'SECRETARIA' | 'TESORERO' | 'PRUEBA';
 
-export interface AuthUser {
-    id: string;
-    nombre: string;
-    rol: UserRole;
-    campo: string;
-    estado: 'Activo' | 'Pendiente';
-    fechaNacimiento?: string;
-    edad?: number | null;
-    grupo?: string;
-}
