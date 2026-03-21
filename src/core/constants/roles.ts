1ef37523216f30f6502cfd12bf9fcd0f0b6b0f31
AppEBD <ebdescueladominical@gmail.com>
2026-03-21 12:00:33 -0600

    Update roles.ts

---
+++ src/core/constants/roles.ts
@@ -0,0 +1,14 @@
+export const ROLES_CONFIG = [
+    { id: 'MAESTRO', name: 'Maestro', icon: 'fa-chalkboard-user' },
+    { id: 'AUXILIAR', name: 'Auxiliar', icon: 'fa-hands-helping' },
+    { id: 'LOGISTICA', name: 'Logística', icon: 'fa-truck-ramp-box' },
+    { id: 'SECRETARIA', name: 'Secretaría', icon: 'fa-file-signature' },
+    { id: 'TESORERO', name: 'Tesorero', icon: 'fa-sack-dollar' },
+    { id: 'ADMIN', name: 'Director', icon: 'fa-user-tie' },
+];
+
+export const IGLESIAS_CAMPOS = [
+    "La Isla", "Las Delicias", "El Amatal", "El Manguito",
+    "Buenos Aires", "Corozal #1", "El Porvenir", "El Caulote",
+    "Corozal #2", "Valle Encantado", "La Playa"
+];
