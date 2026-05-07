15ca2ad61fcbab6cb91854305b73e0a12dbf7f41
AppEBD <ebdescueladominical@gmail.com>
2026-05-07 17:43:58 -0600

    Update auth.model.ts
    error

--- src/modules/auth/domain/auth.model.ts
+++ src/modules/auth/domain/auth.model.ts
@@ -1,4 +1,3 @@
-
 export type UserRole = 'ADMIN' | 'MAESTRO' | 'AUXILIAR' | 'LOGISTICA' | 'SECRETARIA' | 'TESORERO' | 'PRUEBA';
 
 export interface AuthUser {
@@ -11,4 +10,6 @@ export interface AuthUser {
     edad?: number | null;
     grupo?: string;
     clase?: string;
+    fechaInicioServicio?: string; // NUEVO: Fecha en la que inició el ministerio
+    insignias?: string[]; // NUEVO: Array para guardar los ID de las insignias asignadas
 }
