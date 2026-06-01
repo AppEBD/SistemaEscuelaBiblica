31fbc7d02fe6acd1c73e346b05038d6f09a81455
AppEBD <ebdescueladominical@gmail.com>
2026-06-01 11:42:23 -0600

    Update App.tsx
    vista secretaria

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import { useAuth } from '../modules/auth/application/useAuth';
 import { LoginView } from '../modules/auth/presentation/LoginView';
 import { AdminDashboard } from '../modules/admin/presentation/AdminDashboard';
 import { StudentsView } from '../modules/students/presentation/StudentsView';
+import { SecretariaDashboard } from '../modules/finance/presentation/SecretariaDashboard'; // Importamos la nueva vista
 import './App.css';
 
 const App: React.FC = () => {
@@ -13,16 +14,13 @@ const App: React.FC = () => {
     return <LoginView />;
   }
 
-  // Verificamos si es Maestro o Auxiliar para aplicar el diseño nuevo
-  const esMaestroOAuxiliar = userRole === 'MAESTRO' || userRole === 'AUXILIAR';
+  // Verificamos quiénes tienen ya su diseño finalizado
+  const usaNuevoDiseno = userRole === 'MAESTRO' || userRole === 'AUXILIAR' || userRole === 'SECRETARIA';
 
   return (
     <div>
-      {/* Ocultamos el encabezado antiguo SÓLO para Maestros y Auxiliares.
-        Ellos ya tienen el nuevo encabezado global y su menú de perfil.
-        Próximamente, cuando actualicemos al Admin, quitaremos esto por completo.
-      */}
-      {!esMaestroOAuxiliar && (
+      {/* Ocultamos el encabezado antiguo para los que ya tienen su menú nuevo */}
+      {!usaNuevoDiseno && (
         <header className="app-header">
           <div className="app-header-info">
             <h1>EBD v2.0</h1>
@@ -34,17 +32,15 @@ const App: React.FC = () => {
         </header>
       )}
 
-      {/* Si es maestro, le quitamos la clase 'app-main' para que su diseño ocupe toda la pantalla */}
-      <main className={esMaestroOAuxiliar ? '' : 'app-main'}>
+      {/* Si usan nuevo diseño, ocupan toda la pantalla */}
+      <main className={usaNuevoDiseno ? '' : 'app-main'}>
         
-        {/* Vista para el Director */}
         {userRole === 'ADMIN' && <AdminDashboard />}
+        {(userRole === 'MAESTRO' || userRole === 'AUXILIAR') && <StudentsView />}
+        {userRole === 'SECRETARIA' && <SecretariaDashboard />}
         
-        {/* Vista para Maestros y Auxiliares (Con su nuevo diseño integrado) */}
-        {esMaestroOAuxiliar && <StudentsView />}
-        
-        {/* Placeholder para los demás roles */}
-        {(userRole === 'LOGISTICA' || userRole === 'SECRETARIA' || userRole === 'TESORERO') && (
+        {/* Placeholder para Logística y Tesorero que aún están pendientes */}
+        {(userRole === 'LOGISTICA' || userRole === 'TESORERO') && (
             <div style={{ background: 'white', padding: '30px', borderRadius: '20px', textAlign: 'center', marginTop: '20px' }}>
                 <h2>¡Bienvenido, {userData?.nombre}!</h2>
                 <p>Tu panel de {userRole.toLowerCase()} está en construcción.</p>
