ab8cce8b1e6e544afceb8d1b7f9399dd95ab9aff
AppEBD <ebdescueladominical@gmail.com>
2026-06-15 10:30:45 -0600

    Update finance.model.ts

--- src/modules/finance/domain/finance.model.ts
+++ src/modules/finance/domain/finance.model.ts
@@ -5,6 +5,7 @@ export interface Transaccion {
     motivo: string;
     descripcion: string;
     fecha: string;
+    hora: string; // Agregamos la hora exacta de la transacción
     registradoPor: string;
     createdAt: number;
 }
